bf3bfe13588cd5aa7b030ee0764435733bbc0a79
Thiago <thiagoo_gl@hotmail.com>
2020-11-06 09:08:56 -0300

    refatorando nome controller

--- src/main/java/com/mercadolivre/mercadolivre/FluxoCompra/FechamentoComprasAuxiliarController.java
+++ src/main/java/com/mercadolivre/mercadolivre/FluxoCompra/FechamentoComprasAuxiliarController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class FechamentoContasAuxiliarController {
+public class FechamentoComprasAuxiliarController {
 	
 	@PersistenceContext
 	private EntityManager manager;
